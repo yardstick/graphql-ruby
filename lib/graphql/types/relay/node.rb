007f417e7953d8e5618a718b77a3b9446c2d1d5c
Daniel Huckstep <danielh@getyardstick.com>
2019-02-06 17:14:40 -0400

    Use our own ID field
    There's seemingly no way to customize it.

--- lib/graphql/types/relay/node.rb
+++ lib/graphql/types/relay/node.rb
@@ -10,7 +10,7 @@ module GraphQL
         include Types::Relay::BaseInterface
         default_relay(true)
         description "An object with an ID."
-        field(:id, ID, null: false, description: "ID of the object.")
+        field(:gid, ID, null: false, description: "ID of the object.")
       end
     end
   end
